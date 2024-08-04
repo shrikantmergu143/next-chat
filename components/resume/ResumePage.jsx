7edf879aa99df8c5131796ae2deb5427a034e979
shrikant <shrikant@appristine.in>
2024-08-04 21:00:37 +0530

    pushing code

--- components/resume/ResumePage.jsx
+++ components/resume/ResumePage.jsx
@@ -57,10 +57,9 @@ export default function ResumePage() {
         <div className='resume-page' id='white-theme'>
           <page className={'page'}>
               <Row className='m-0'>
-                <Col className='p-0 col-6'>
+                <Col className='col-6'>
                   <h5>SHRIKANT MERGU</h5>
-                  <AnchorLink to={'https://www.linkedin.com/in/shrikant-mergu-53962b1a6/'} target={"_blank"} className={"d-block"}>linkedin.com/shrikant-mergu-53962b1a6</AnchorLink>
-                  <AnchorLink to={"https://github.com/shrikantmergu143"} target={"_blank"} className={"d-block"}>github.com/shrikantmergu143</AnchorLink>
+                  <h6 className='mb-0'>Sr Software Developer</h6>
                 </Col>
                 <Col className='px-0 pt-2 d-flex flex-column align-items-end col-6'>
                   <table border={0}>
@@ -68,21 +67,112 @@ export default function ResumePage() {
                       <th>
                         Email:
                       </th>
-                      <td>
+                      <td align={"right"}>
                         <AnchorLink to={"mailto:shrikantmergu123@gmail.com"} target={"_blank"} >shrikantmergu123@gmail.com</AnchorLink>
                       </td>
                     </tr>
                     <tr>
                       <th>
-                      Mobile:
+                        Mobile:
                       </th>
-                      <td>
+                      <td align={"right"}>
                         <AnchorLink to={"tel:+919028309906"} target >+91-9028309906</AnchorLink>
                       </td>
                     </tr>
                   </table>
                 </Col>
+                {/* <hr/> */}
+                <div className='d-flex justify-content-center pt-2'>
+                  {/* <span className={"px-2"}>267/58 Raviwar Peth, Solapur Maharashtra</span> */}
+                  <AnchorLink to={'https://www.linkedin.com/in/shrikant-mergu-53962b1a6/'} target={"_blank"} className={"px-2"}>linkedin.com/shrikant-mergu-53962b1a6</AnchorLink>
+                  <AnchorLink to={"https://github.com/shrikantmergu143"} target={"_blank"} className={"px-2"}>github.com/shrikantmergu143</AnchorLink>
+                  <AnchorLink to={"https://www.instagram.com/_.shrikantmergu3006"} target={"_blank"} className={"px-2"}>instagram.com/_.shrikantmergu3006</AnchorLink>
+                </div>
               </Row>
+              <hr/>
+              <div>
+                <h6 className='mb-2'>SUMMARY</h6>
+                <p>Seasoned Senior React Developer with extensive experience in <b className='fw-5'>React.js</b>, <b className='fw-5'>Express.js</b>, and <b className='fw-5'>Next.js</b>. Proven track record of building high-performance web applications and leading development teams. Expert in creating scalable solutions, optimizing user experiences, and leveraging modern <b className='fw-5'>JavaScript</b> technologies to drive innovation and efficiency.</p>
+              </div>
+              <hr/>
+              <div>
+                <h6 className='mb-2'>WORK EXPERIENCE</h6>
+                <div className='pb-2'>
+                  <div className='d-flex'>
+                    <h6 className='title-1'>Jr. PHP Developer | NewSoft Solutions PVT. LTD.</h6>
+                    <h6 className='title-1 ms-auto text-secondary mb-0'>OCT/2021 — DEC/2021</h6>
+                  </div>
+                  <ul>
+                    <li>As a PHP developer is responsible for create <b  className='fw-5'>server-side web application</b> logic.</li>
+                    <li>Usually develop back-end components, connect the application with the other (often third-party) web services, and support the front-end developers by integrating their work with the application.</li>
+                  </ul>
+                </div>
+                <div className='pb-2'>
+                  <div className='d-flex'>
+                    <h6 className='title-1'>Sr. Software Developer | Appristine Technologies PVT. LTD.</h6>
+                    <h6 className='title-1 ms-auto text-secondary mb-0'>DEC/2021 — Present</h6>
+                  </div>
+                  <ul className='mb-0'>
+                    <li>
+                      Contributed to the creation of <b className='fw-5'>reusable functional components</b> with React Hooks, reducing code redundancy and encouraging best practices across the team.
+                    </li>
+                    <li>
+                      Designed and implemented scalable server-side applications with <span className='fw-5'>Express.js</span> and Laravel, ensuring robust <span className='fw-5'>back-end functionality</span> and efficient data handling.
+                    </li>
+                    <li>
+                      Utilized Vite for fast builds and improved front-end performance, integrating with Redux for state management and Inertia for seamless client-server communication.
+                    </li>
+                    <li>
+                      Worked closely with cross-functional teams to define project requirements, <span className='fw-5'> deliverhigh-quality code</span>, and ensure timely project delivery. Participated in the revamp of a <span className='fw-5'>core application feature using Redux</span>, which normalized state shape and reduced state management complexities.
+                    </li>
+                    <li>
+                      Implemented best practices in coding, conducted code reviews, and optimized existing codebase for performance and maintainability.
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <hr/>
+              <div>
+                <h6 className='mb-2'>EDUCATION</h6>
+                <div className='pb-2'>
+                  <div className='d-flex'>
+                    <h6 className='title-1'>Master of Computer Application<span className='ms-1 fw-5 text-secondary-1'>| Percentage - 60.20%</span></h6>
+                    <h6 className='title-1 ms-auto text-secondary mb-0'>DEC/2021 — APR/2024</h6>
+                  </div>
+                  <h6 className='title-2 text-secondary'>Punyashlok Ahilyadevi Holkar University, Solapur</h6>
+                </div>
+                <div className='pb-2'>
+                  <div className='d-flex'>
+                    <h6 className='title-1'>Bachelor of Computer Application<span className='ms-1 fw-5 text-secondary-1'>| Percentage - 67.53%</span></h6>
+                    <h6 className='title-1 ms-auto text-secondary mb-0'>JUN/2018 — MAR/2021</h6>
+                  </div>
+                  <h6 className='title-2 text-secondary'>Hirachand Nemchand College of Commerce, Solapur</h6>
+                </div>
+                <div className='pb-2'>
+                  <div className='d-flex'>
+                    <h6 className='title-1'>12th STD | HSC (Commerce)<span className='ms-1 fw-5 text-secondary-1'>| Percentage - 66.92%</span></h6>
+                    <h6 className='title-1 ms-auto text-secondary mb-0'>JUN/2015 — MAR/2017</h6>
+                  </div>
+                  <h6 className='title-2 text-secondary'>Kuchan Junior College, Solapur</h6>
+                </div>
+                <div className='pb-2'>
+                  <div className='d-flex'>
+                    <h6 className='title-1'>10th STD | SSC (Commerce)<span className='ms-1 fw-5 text-secondary-1'>| Percentage - 52.40%</span></h6>
+                    <h6 className='title-1 ms-auto text-secondary mb-0'>JUN/2013 — MAR/2014</h6>
+                  </div>
+                  <h6 className='title-2 text-secondary'>Shri Markandeya Highschool, Solapur</h6>
+                </div>
+              </div>
+              <div>
+                <h6 className='mb-2'>ADDITIONAL INFORMATION</h6>
+                <div className='pb-2'>
+                  <span>
+                      <h6 className='title-2 d-inline fw-7'>Technical Skills:
+                      <p className='mb-0 d-inline ps-2 fw-5'>React.js, Next.js, Node.js, Vite.js, PHP, MongoDB, Laravel, Mysqli, HTML, CSS, JavaScript, Material UI, TypeScript.</p>
+                      </h6>
+                  </span>
+                </div>
+              </div>
           </page>
         </div>
         <Button onClick={generateAndDownloadPdf}>Download</Button>
